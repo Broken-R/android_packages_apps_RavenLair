b27317fe8a188ec731fe6251a8e13f305872266b
Zaviyar786 <beingme639@gmail.com>
2020-11-25 02:33:35 +0530

    Team : Update Github Id
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- src/com/raven/lair/fragments/team/TeamActivity.java
+++ src/com/raven/lair/fragments/team/TeamActivity.java
@@ -58,7 +58,7 @@ public class TeamActivity extends Activity {
                 + " / " + getString(R.string.maintainer_title), "DeadmanxXD", "deadmanxxd",
                 R.drawable.deadmanxxd);
        setTeamMember("Zeeshan", getString(R.string.developer_title)
-                + " / " + getString(R.string.maintainer_title), "HeartStealer786", "NAHSEEZ", 
+                + " / " + getString(R.string.maintainer_title), "Zaviyar786", "NAHSEEZ", 
                 R.drawable.zeeshan);
        setTeamMember("Aashil", getString(R.string.developer_title), "aashil123", "tempyourdaddy",
                 R.drawable.ashil);       
